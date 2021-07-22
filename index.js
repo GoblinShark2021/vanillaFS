64d663de3541c270f5843ca54fe4649300b4c1c7
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-21 21:27:33 -0500

    front end is talking to the backend

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 /* eslint-disable no-undef */
+
 //set a variable equal to the body
 const body = document.querySelector('body');
+//spongebob button
 const clickBtn = document.createElement('button');
-const dataTable = [];
-
 body.appendChild(clickBtn).innerHTML = 'Change Sponge Size';
 
-let numberOfClicks = 0;
+//backend testing button
+const backendBtn = document.createElement('button');
+body.appendChild(backendBtn).innerHTML = 'Backend Tester';
 
+//functions for our scripts
 function changeSize(i) {
   console.log('spongebob has shrunk');
   document.getElementById('spongebob').style.height = `${
     Math.random() * 1000
   }px`;
-  dataTable.push(numberOfClicks);
-  console.log(dataTable);
-  numberOfClicks++;
-  const newh1 = document.createElement('h1');
-  body.appendChild(newh1).innerHTML = `${numberOfClicks}`;
 }
 
+function beTest() {
+  axios
+    .get('/test')
+    .then((res) => {
+      console.log('talkin 2 tha back', res.data);
+    })
+    .catch((error) => {
+      console.log('error:', error);
+    });
+}
+
+//event listeners
 clickBtn.addEventListener('click', changeSize);
+backendBtn.addEventListener('click', beTest);
--- server.js
+++ server.js
@@ -4,14 +4,31 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+//middleware
 app.use(express.json());
 
+//serve static css files, etc
 app.use(express.static(__dirname + '/'));
 
+app.get('/test', function (req, res) {
+  console.log('good request');
+  res.status(200).send('You on the backend playa');
+});
+
+//root get request
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+/**
+ * Global error handler
+ */
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+//server start with listening
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
